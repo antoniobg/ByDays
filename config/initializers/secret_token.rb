15a751bdbe42d1500297ebd81cbc9d30cd8e7ae9
Antonio Borrero Granell <abogra1987@gmail.com>
2014-09-28 19:58:17 +0200

    Update gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ByDays::Application.config.secret_key_base = '3d2b6898e166c4a6a4cc0230098aff03ddf7cf58e19e2b0a3836d7918f94262af2c7741eea4f81c0ac1a57c340b13599191fdd814fbb81cb9a3e7141b6f4220d'
